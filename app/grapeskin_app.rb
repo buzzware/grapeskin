9fdd39959871b9c5baee98493a2b8be958e095d7
Gary McGhee <gary@buzzware.com.au>
2015-01-15 08:48:30 +0800

    allow post via cors

--- app/grapeskin_app.rb
+++ app/grapeskin_app.rb
@@ -127,15 +127,16 @@ module Grapeskin
       #   )
     end
 
+
+    # !!! Maybe we should move Rack::Cors into the Apis, and support standard rack apps (not just Grape) eg. Rack::Builder apps
     def self.instance
       @instance ||= Rack::Builder.new do
-        use Rack::Cors do
+        use Rack::Cors, debug: true, logger: Logger.new(STDOUT) do
           allow do
             origins '*'
-            resource '*', headers: :any, methods: :get
+            resource '*', headers: :any, methods: [:get, :post]
           end
         end
-
         run Grapeskin::App.new
       end.to_app
     end
@@ -171,27 +172,5 @@ module Grapeskin
       #cls.endpoints.each {|e| e.options[:path] ||= "/#{app_name}"
       cls.call(env)
     end
-
-    # api
-    # response = Grapeskin::API.call(env)
-
-    # # Check if the App wants us to pass the response along to others
-    # if response[1]['X-Cascade'] == 'pass'
-    #   # static files
-    #   request_path = env['PATH_INFO']
-    #   @filenames.each do |path|
-    #     response = @rack_static.call(env.merge('PATH_INFO' => request_path + path))
-    #     return response if response[0] != 404
-    #   end
-    # end
-    #
-    # # Serve error pages or respond with API response
-    # case response[0]
-    # when 404, 500
-    #   content = @rack_static.call(env.merge('PATH_INFO' => "/errors/#{response[0]}.html"))
-    #   [response[0], content[1], content[2]]
-    # else
-    #  response
-    # end
   end
 end
